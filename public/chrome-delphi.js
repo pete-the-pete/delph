f458409e1db9b84f18304a2b832e21535527c7de
pliljegr <pliljegr@linkedin.com>
2015-02-04 22:12:15 -0800

    Small clean up of project, adding files to open a tab with ember instead of a popup

---
+++ public/chrome-delphi.js
@@ -0,0 +1,6 @@
+/**
+* Listen for clicks on the main extension button.
+*/
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({'url': chrome.extension.getURL('index.html')});
+});
