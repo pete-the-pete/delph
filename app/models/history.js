8aa6d1d55453bbc00d591f321d2aabf093856bde
pliljegr <pliljegr@linkedin.com>
2015-02-06 09:57:45 -0800

    Adding chrome histroy model and adapter.

---
+++ app/models/history.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  'chromeId': DS.attr('number'),
+  'lastVisitTime': DS.attr('number'),
+  'title': DS.attr('string'),
+  'typedCount': DS.attr('number'),
+  'url': DS.attr('string'),
+  'visitCount': DS.attr('number')
+});
