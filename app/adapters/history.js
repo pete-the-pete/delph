6b9b70dbfe250fcd3f78a1fdc83d175d3ec27f8d
pliljegr <pliljegr@linkedin.com>
2015-02-09 08:04:44 -0800

    Updating history adapter to use injected chrome variable, adding tests

--- app/adapters/history.js
+++ app/adapters/history.js
@@ -1,24 +1,22 @@
 import Ember from 'ember';
 import DS from "ember-data";
 
-var ALL_HISTORY = {'text':''};
-
 export default DS.Adapter.extend({
   defaultSerializer: '-default',
 
-  //shortcuts to the chrome functions
-  c_history: chrome.history,
+  ALL_HISTORY: {'text':''},
 
   find: function(store, type, id) {
     this._super(store, type, id);
   },
 
   findAll: function(store, type) {
-    return this.findQuery(store, type, ALL_HISTORY);
+    return this.findQuery(store, type, this.ALL_HISTORY);
   },
   findQuery: function(store, type, query) {
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      chrome.history.search(query, function(historyItems) {
+    var c = this.chrome;
+    return new Ember.RSVP.Promise(function(resolve) {
+      c.history.search(query, function(historyItems) {
         resolve(historyItems);    
       });
     });
---
+++ tests/unit/adapters/history-test.js
@@ -0,0 +1,26 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+import Ember from "ember";
+
+var HistoryItem, env, store, adapter, chrome;
+
+moduleFor('adapter:history', 'HistoryAdapter', {
+});
+
+// Replace this with your real tests.
+test('it exists', function() {
+  var adapter = this.subject();
+  ok(adapter);
+});
+
+test('findAll should use the default query', function() {
+  var adapter = this.subject();
+  adapter.findQuery = function(store, type, query) {
+    equal(query, adapter.ALL_HISTORY);
+    return Ember.RSVP.resolve();
+  };
+
+  adapter.findAll();
+});
