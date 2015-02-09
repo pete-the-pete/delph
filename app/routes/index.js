59f47a3b6bbe2791c93bf3613edad46726c07583
pliljegr <pliljegr@linkedin.com>
2015-02-09 08:27:00 -0800

    Simple display of data retrieved by adapter

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,15 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('history');
-  },
-  afterModel: function(historyItems) {
-    console.debug(historyItems);
   }
-/*  setupController: function(controller, model) {
-    if(chrome.history) {
-        chrome.history.search({'text': ''}, function(historyItems) {
-          controller.set('historyItems', historyItems);
-        });
-    }
-  }*/
 });
