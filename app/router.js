f37b2e4a0d93fcbcfba9d5eb4b89e4a24978870a
pliljegr <pliljegr@linkedin.com>
2015-02-19 09:01:07 -0800

    Renaming the index contorl to analytics, and making it the default route

--- app/router.js
+++ app/router.js
@@ -6,8 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('about', { path: '/'});
-  this.route('analytics');
+  this.route('about');
+  this.route('analytics', {path: '/'});
 });
 
 export default Router;
